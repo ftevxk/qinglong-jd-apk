5f901277ea489592e538c926603db1bb4a2b7ad2
ftevxk <2116899562@qq.com>
2025-07-05 14:58:52 +0800

    更新青龙面板配置

--- app/src/main/java/cn/moon/ql/App.java
+++ app/src/main/java/cn/moon/ql/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
 
     private void initApp() {
         sharedPreferences = getSharedPreferences(QL_DATA_FILE_NAME, MODE_PRIVATE);
-        String url = sharedPreferences.getString("url", "http://2833fa3b.r23.cpolar.top");
+        String url = sharedPreferences.getString("url", "http://ql.free.idcfengye.com");
         String cid = sharedPreferences.getString("cid", "f7kMA-5m_vmW");
         String csk = sharedPreferences.getString("csk", "sPO3x8Vg3i0Y240TmMzrAu-H");
 
